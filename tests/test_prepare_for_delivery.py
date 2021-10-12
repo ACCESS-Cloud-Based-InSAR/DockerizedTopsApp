ed1e14bd5041ba6f23bc5eec3059c4fd137bce76
cmarshak <charles.marshak@gmail.com>
2021-10-12 12:48:56 -0700

    additional tests for delivery and water mask

---
+++ tests/test_prepare_for_delivery.py
@@ -0,0 +1,16 @@
+import papermill as pm
+from pathlib import Path
+
+
+def test_download_dem():
+
+    test_dir = Path(__file__).parents[0].absolute()
+
+    out_dir = test_dir/'out'
+    out_dir.mkdir(exist_ok=True)
+
+    out_notebook = out_dir/'prepare-for-delivery.ipynb'
+
+    pm.execute_notebook(test_dir/'prepare-for-delivery.ipynb',
+                        output_path=out_notebook
+                        )
