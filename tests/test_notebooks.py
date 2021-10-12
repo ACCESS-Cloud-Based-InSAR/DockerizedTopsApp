0e329c8fb3d84a78b758c2cdcb225d0bad3a0c76
cmarshak <charles.marshak@gmail.com>
2021-10-12 13:58:15 -0700

    combine papermill tests

---
+++ tests/test_notebooks.py
@@ -0,0 +1,21 @@
+import papermill as pm
+from pathlib import Path
+import pytest
+
+notebooks = ['localize-data.ipynb',
+             'prepare-for-delivery.ipynb']
+
+
+@pytest.mark.parametrize('notebook_name', notebooks)
+def test_notebooks(notebook_name):
+
+    test_dir = Path(__file__).parents[0].absolute()
+
+    out_dir = test_dir/'out'
+    out_dir.mkdir(exist_ok=True)
+
+    out_notebook = out_dir/f'{notebook_name}'
+
+    pm.execute_notebook(test_dir/f'{notebook_name}',
+                        output_path=out_notebook
+                        )
