789d714bc54cc2e70babfbd79aa8db9573c7b3c3
Joseph H Kennedy <me@jhkennedy.org>
2022-03-11 15:28:14 -0900

    whitespace

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -11,11 +11,11 @@ def test_notebooks(notebook_name):
 
     test_dir = Path(__file__).parents[0].absolute()
 
-    out_dir = test_dir/'out'
+    out_dir = test_dir / 'out'
     out_dir.mkdir(exist_ok=True)
 
-    out_notebook = out_dir/f'{notebook_name}'
+    out_notebook = out_dir / f'{notebook_name}'
 
-    pm.execute_notebook(test_dir/f'{notebook_name}',
+    pm.execute_notebook(test_dir / f'{notebook_name}',
                         output_path=out_notebook
                         )
