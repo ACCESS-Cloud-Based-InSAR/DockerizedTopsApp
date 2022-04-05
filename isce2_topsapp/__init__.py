87f70eaae5ec3876e28dc5f6e041bd53b2513449
Joseph H Kennedy <me@jhkennedy.org>
2022-04-04 18:16:47 -0800

    update packaging

--- isce2_topsapp/__init__.py
+++ isce2_topsapp/__init__.py
@@ -1,3 +1,6 @@
+import warnings
+from importlib.metadata import PackageNotFoundError, version
+
 from .delivery_prep import prepare_for_delivery
 from .localize_aux_cal import download_aux_cal
 from .localize_dem import download_dem_for_isce2
@@ -6,10 +9,21 @@ from .localize_slc import download_slcs
 from .packaging import package_gunw_product
 from .topsapp_proc import topsapp_processing
 
-__all__ = ['download_orbits',
-           'download_slcs',
-           'download_dem_for_isce2',
-           'download_aux_cal',
-           'topsapp_processing',
-           'package_gunw_product',
-           'prepare_for_delivery']
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    __version__ = None
+    warnings.warn('package is not installed!\n'
+                  'Install in editable/develop mode via (from the top of this repo):\n'
+                  '   python -m pip install -e .\n', RuntimeWarning)
+
+__all__ = [
+    'download_orbits',
+    'download_slcs',
+    'download_dem_for_isce2',
+    'download_aux_cal',
+    'topsapp_processing',
+    'package_gunw_product',
+    'prepare_for_delivery',
+]
