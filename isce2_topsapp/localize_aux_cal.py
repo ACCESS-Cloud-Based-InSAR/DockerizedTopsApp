f042080099500dc19ed5836e5e484ced693196c0
cmarshak <charles.marshak@gmail.com>
2022-06-14 12:51:53 -0700

    change tarfile to zipfile and remove final backslash

--- isce2_topsapp/localize_aux_cal.py
+++ isce2_topsapp/localize_aux_cal.py
@@ -1,11 +1,11 @@
-import tarfile
+import zipfile
 from pathlib import Path
 from typing import Union
 
 import requests
 
-S1A_AUX_URL = 'https://sar-mpc.eu/download/55282da1-679d-4ecf-aeef-d06b024451cf/'
-S1B_AUX_URL = 'https://sar-mpc.eu/download/3c8b7c8d-d3de-4381-a19d-7611fb8734b9/'
+S1A_AUX_URL = 'https://sar-mpc.eu/download/55282da1-679d-4ecf-aeef-d06b024451cf'
+S1B_AUX_URL = 'https://sar-mpc.eu/download/3c8b7c8d-d3de-4381-a19d-7611fb8734b9'
 
 
 def download_aux_cal(aux_cal_dir: Union[str, Path] = None):
@@ -25,9 +25,9 @@ def download_aux_cal(aux_cal_dir: Union[str, Path] = None):
     s1a_path = download_one(S1A_AUX_URL)
     s1b_path = download_one(S1B_AUX_URL)
 
-    with tarfile.open(s1a_path) as tar:
-        tar.extractall(path=aux_cal_dir)
-    with tarfile.open(s1b_path) as tar:
-        tar.extractall(path=aux_cal_dir)
+    with zipfile.ZipFile(s1a_path) as zip_file:
+        zip_file.extractall(aux_cal_dir)
+    with zipfile.ZipFile(s1b_path) as zip_file:
+        zip_file.extractall(aux_cal_dir)
 
     return {'aux_cal_dir': str(aux_cal_dir)}
