09ef00c01c72706b8dfdf8d71dd005b5ef1c3d35
Forrest Williams <ffwilliams2@alaska.edu>
2022-09-27 10:14:11 -0500

    add tests and burst_request function

--- isce2_topsapp/burstio.py
+++ isce2_topsapp/burstio.py
@@ -1,21 +1,43 @@
+import os
 import xml.etree.ElementTree as ET
 from pathlib import Path
 
 import pandas as pd
-
 from shapely import geometry
 
 
-def create_job_xml(reference_safe, secondary_safe, swath, polarization, bbox):
-    config = f'''
-    <?xml version="1.0" encoding="UTF-8"?>
+def generate_burst_request(safe_url, image_number, burst_number, content):
+    token = os.environ['EDL_TOKEN']
+    urls = {
+        'metadata': 'https://g6rmelgj3m.execute-api.us-west-2.amazonaws.com/metadata',
+        'data': 'https://g6rmelgj3m.execute-api.us-west-2.amazonaws.com/metadata',
+    }
+
+    url = urls[content]
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Referer': 't',
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+    data = f'zip_url={safe_url}&image_number={image_number}&burst_number={burst_number}'
+    return {'url': url, 'headers': headers, 'data': data}
+
+
+def create_job_xml(reference_safe, secondary_safe, swath, polarization, bbox, do_esd, range_looks, azimuth_looks):
+    geocode_list = [
+        'merged/phsig.cor',
+        'merged/filt_topophase.unw',
+        'merged/los.rdr',
+        'merged/topophase.flat',
+        'merged/filt_topophase.flat',
+        'merged/topophase.cor',
+        'merged/filt_topophase.unw.conncomp',
+    ]
+
+    config = f'''<?xml version="1.0" encoding="UTF-8"?>
     <topsApp>
         <component name="topsinsar">
-            <property name="do unwrap">True</property>
-            <property name="unwrapper name">snaphu_mcf</property>
-            <property name="swaths">[{swath}]</property>
-            <property name="do ESD">False</property>
-            <property name="region of interest">{bbox}</property>
+            <property name="Sensor name">SENTINEL1</property>
             <component name="reference">
                 <property name="output directory">reference</property>
                 <property name="polarization">'{polarization.lower()}'</property>
@@ -26,6 +48,15 @@ def create_job_xml(reference_safe, secondary_safe, swath, polarization, bbox):
                 <property name="polarization">'{polarization.lower()}'</property>
                 <property name="safe">{secondary_safe}</property>
             </component>
+            <property name="swaths">[{swath}]</property>
+            <property name="range looks">7</property>
+            <property name="azimuth looks">3</property>
+            <property name="region of interest">{bbox}</property>
+            <property name="do denseoffsets">True</property>
+            <property name="do ESD">{do_esd}</property>
+            <property name="do unwrap">True</property>
+            <property name="unwrapper name">snaphu_mcf</property>
+            <property name="geocode list">{geocode_list}</property>
         </component>
     </topsApp>
     '''
@@ -40,7 +71,8 @@ def reformat_gcp(point):
     return values
 
 
-def create_gcp_df(points):
+def create_gcp_df(annotation):
+    points = annotation.findall('.//{*}geolocationGridPoint')
     gcp_df = pd.DataFrame([reformat_gcp(x) for x in points])
     gcp_df = gcp_df.sort_values(['line', 'pixel']).reset_index(drop=True)
     return gcp_df
@@ -77,7 +109,10 @@ if __name__ == '__main__':
     home_dir = Path('.') / 'isce2_bursts'
     reference = ''
     secondary = ''
-    filename = './filename/burst_script/S1A_IW_SLC__1SDV_20200604T022251_20200604T022318_032861_03CE65_7C85.SAFE/annotation/s1a-iw2-slc-vh-20200604t022253-20200604t022318-032861-03ce65-002.xml'
+    filename = (
+        './filename/burst_script/S1A_IW_SLC__1SDV_20200604T022251_20200604T022318_032861_03CE65_7C85.SAFE/'
+        'annotation/s1a-iw2-slc-vh-20200604t022253-20200604t022318-032861-03ce65-002.xml'
+    )
     burst_index = 1  # we're 1 indexed most of the time
     swath = 2
 
--- tests/test_burstio.py
+++ tests/test_burstio.py
@@ -1,3 +1,54 @@
+import xml.etree.ElementTree as ET
+from pathlib import Path
+
+import numpy as np
+import pytest
+import requests
+
 from isce2_topsapp import burstio
 
 
+@pytest.fixture(scope='module')
+def annotation():
+    annotation_path = Path(__file__).parent.absolute() / 'test_data' / 'annotation.xml'
+    annotation = ET.parse(annotation_path).getroot()
+    return annotation
+
+
+def test_create_gcp_df(annotation):
+    n_bursts = int(annotation.findall('.//burstList')[0].attrib['count'])
+    lines_per_burst = int(annotation.findtext('.//{*}linesPerBurst'))
+    gcp_df = burstio.create_gcp_df(annotation)
+    assert np.all(gcp_df.columns == ['line', 'pixel', 'latitude', 'longitude', 'height'])
+    assert gcp_df.line.min() == 0
+    assert gcp_df.line.max() == (n_bursts * lines_per_burst) - 1
+
+
+@pytest.mark.parametrize(
+    'index, real_box',
+    [
+        (0, (53.39892830515005, 28.68119969132844, 54.3716989017821, 28.99795274730827)),
+        (8, (53.17067752190982, 27.51599975559423, 54.09849123017585, 27.66669346258735)),
+    ],
+)
+def test_create_geometry(index, real_box, annotation):
+    lines_per_burst = int(annotation.findtext('.//{*}linesPerBurst'))
+    gcp_df = burstio.create_gcp_df(annotation)
+    box = burstio.create_geometry(gcp_df, index, lines_per_burst)[1]
+    assert np.all([np.isclose(a, b) for a, b in zip(box, real_box)])
+
+
+def test_create_job_xml():
+    job_xml = burstio.create_job_xml('a.SAFE', 'b.SAFE', 2, 'vv', [0, 1, 2, 3], False, 3, 7)
+    assert ET.fromstring(job_xml)
+
+
+def test_generate_burst_request():
+    safe_url = (
+        "https://datapool.asf.alaska.edu/SLC/SA/S1A_IW_SLC__1SDV_20200604T022251_20200604T022318_032861_03CE65_7C85.zip"
+    )
+    image_number = 2
+    burst_number = 3
+    params = burstio.generate_burst_request(safe_url, image_number, burst_number, 'metadata')
+    response = requests.head(**params)
+    assert response.ok
