ff9766393c2dc6957bf76d21aa042dd4723442cd
Charlie Marshak <charles.marshak@gmail.com>
2022-10-26 13:38:06 -0700

    finalize packaging

--- isce2_topsapp/packaging_utils/additional_layers.py
+++ isce2_topsapp/packaging_utils/additional_layers.py
@@ -1,7 +1,6 @@
 import json
 from pathlib import Path
 
-import rioxarray as rxr
 import xarray as xr
 
 LAYER_JSON = Path(__file__).parents[0] / 'additional_layers.json'
@@ -11,20 +10,21 @@ ADDITIONAL_LAYERS = json.load(open(LAYER_JSON))
 def add_2d_layer(layer_name: str, netcdf_path: Path) -> Path:
 
     layer_data = ADDITIONAL_LAYERS[layer_name]
-    data_array = rxr.open_rasterio(layer_data['input_relative_path'])
-    data_array = data_array.rename({'x': 'longitude',
-                                    'y': 'latitude'})
 
-    data_array = data_array.squeeze(dim=['band'],
-                                    drop=True)
+    ds = xr.open_dataset(layer_data['input_relative_path'],
+                         engine='rasterio')
+    ds = ds.rename({'x': 'longitude',
+                    'y': 'latitude',
+                    'band_data': layer_name})
+    ds['latitude'].attrs.update({'long_name': 'latitude',
+                                 'standard_name': 'latitude'})
+    ds['longitude'].attrs.update({'long_name': 'longitude',
+                                  'standard_name': 'longitude'})
+    ds = ds.squeeze(['band'], drop=True)
+    ds[layer_name].attrs.update(layer_data['attrs'])
 
-    data_array.attrs.update(layer_data['attr'])
-    group = xr.Dataset({layer_name: data_array})
-
-    group.rio.write_crs(4326, inplace=True)
-
-    group.to_netcdf(netcdf_path,
-                    group=layer_data['dst_group'],
-                    mode='a')
+    ds.to_netcdf(netcdf_path,
+                 group=layer_data['dst_group'],
+                 mode='a')
 
     return netcdf_path
