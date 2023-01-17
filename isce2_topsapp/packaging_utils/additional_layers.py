0b405a1a4e7f7f0eca02a033e800d1159147af33
Charlie Marshak <charles.marshak@gmail.com>
2023-01-17 10:54:22 -0800

    ensure default placeholders in packaging; consistency; Buzzanga H5 overwrite

--- isce2_topsapp/packaging_utils/additional_layers.py
+++ isce2_topsapp/packaging_utils/additional_layers.py
@@ -1,6 +1,7 @@
 import json
 from pathlib import Path
 
+import h5py
 import xarray as xr
 
 LAYER_JSON = Path(__file__).parents[0] / 'additional_layers.json'
@@ -8,8 +9,21 @@ ADDITIONAL_LAYERS = json.load(open(LAYER_JSON))
 
 
 def add_2d_layer(layer_name: str, netcdf_path: Path) -> Path:
+    """
+    Combines a lot of standard formatting of the netcdf via rioxarray and
+    deletes the previous placeholder (we assume it exists via the placeholder).
+
+    We also assume any additional processing specific to GUNW is done outside of
+    this function.
+    """
 
     layer_data = ADDITIONAL_LAYERS[layer_name]
+    dst_group = layer_data['dst_group']
+    dst_variable = layer_data['dst_variable']
+
+    # The layers generally already exist within the file
+    with h5py.File(netcdf_path, mode='a') as file:
+        del file[dst_group][dst_variable]
 
     ds = xr.open_dataset(layer_data['input_relative_path'],
                          engine='rasterio')
@@ -17,7 +31,7 @@ def add_2d_layer(layer_name: str, netcdf_path: Path) -> Path:
     # Renaming ensures correct geo-referencing with spatial_ref grid mapping
     ds = ds.rename({'x': 'longitude',
                     'y': 'latitude',
-                    'band_data': layer_name})
+                    'band_data': dst_variable})
     ds['latitude'].attrs.update({'long_name': 'latitude',
                                  'standard_name': 'latitude'})
     ds['longitude'].attrs.update({'long_name': 'longitude',
