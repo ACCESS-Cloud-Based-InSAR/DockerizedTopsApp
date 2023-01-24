1dd64b67f6c0065bfe47f3071bf15c9ed8fb39f9
Charlie Marshak <charles.marshak@gmail.com>
2023-01-24 10:18:42 -0800

    delete sample runs

--- sample_run_frame_19966.sh
+++
@@ -1,7 +0,0 @@
-isce2_topsapp --reference-scenes S1A_IW_SLC__1SDV_20230113T140019_20230113T140046_046766_059B44_A9C1 \
-                                 S1A_IW_SLC__1SDV_20230113T140044_20230113T140111_046766_059B44_FBB8 \
-              --secondary-scenes S1A_IW_SLC__1SDV_20221208T140021_20221208T140048_046241_05897B_8EBC \
-                                 S1A_IW_SLC__1SDV_20221208T140046_20221208T140113_046241_05897B_28A9 \
-              --region-of-interest -121.167298 33.929114 -118.055825 35.904876 \
-              --frame-id 19966 \
-              > topsapp_img_f19966.out 2> topsapp_img_f19966.err
