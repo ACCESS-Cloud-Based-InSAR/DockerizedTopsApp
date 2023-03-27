6346f85aabc99d9e34515696af49604de590f980
cmarshak <charles.marshak@gmail.com>
2023-03-27 13:53:22 -0700

    select band number from isce2 outputs

--- isce2_topsapp/packaging_utils/additional_layers.py
+++ isce2_topsapp/packaging_utils/additional_layers.py
@@ -8,7 +8,8 @@ LAYER_JSON = Path(__file__).parents[0] / 'additional_layers.json'
 ADDITIONAL_LAYERS = json.load(open(LAYER_JSON))
 
 
-def add_2d_layer(layer_name: str, gunw_netcdf_path: Path) -> Path:
+def add_2d_layer(layer_name: str,
+                 gunw_netcdf_path: Path) -> Path:
     """
     Combines a lot of standard formatting of the netcdf via rioxarray and
     deletes the previous placeholder (we assume it exists via the placeholder).
@@ -20,6 +21,10 @@ def add_2d_layer(layer_name: str, gunw_netcdf_path: Path) -> Path:
     layer_data = ADDITIONAL_LAYERS[layer_name]
     dst_group = layer_data['dst_group']
     dst_variable = layer_data['dst_variable']
+    band_number = layer_data['band_number']
+    if not isinstance(band_number, int):
+        ValueError('Layers must select individual layers from outputs i.e '
+                   '1, 2, ...')
 
     # The layers generally already exist within the file
     with h5py.File(gunw_netcdf_path, mode='a') as file:
@@ -35,16 +40,20 @@ def add_2d_layer(layer_name: str, gunw_netcdf_path: Path) -> Path:
                          engine='rasterio')
 
     # Renaming ensures correct geo-referencing with spatial_ref grid mapping
-    ds = ds.rename({'x': 'longitude',
+    ds = ds.rename({
+                    # x, y are the coordinate names
+                    'x': 'longitude',
                     'y': 'latitude',
+                    # band_data is the variable name of the image
                     'band_data': dst_variable})
+    # removes channel (what rioxarray band) dimension in the coordinates
+    ds = ds.sel(band=band_number, drop=True)
+
     ds['latitude'].attrs.update({'long_name': 'latitude',
                                  'standard_name': 'latitude'})
     ds['longitude'].attrs.update({'long_name': 'longitude',
                                   'standard_name': 'longitude'})
 
-    # removes channel (aka band) dimension
-    ds = ds.squeeze(['band'], drop=True)
     ds[layer_name].attrs.update(layer_data['attrs'])
 
     ds.to_netcdf(gunw_netcdf_path,
