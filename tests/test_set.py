cb7cc6804aa43712dd2cb3a9f25070f357afd043
Charlie Marshak <charlie.z.marshak@jpl.nasa.gov>
2023-04-26 17:12:16 -0700

    import test_dir to nb and azimuth time test

--- tests/test_set.py
+++ tests/test_set.py
@@ -3,8 +3,11 @@ import shutil
 import rasterio
 from affine import Affine
 from rasterio.crs import CRS
+import xarray as xr
+import numpy as np
+from numpy.testing import assert_almost_equal
 
-from isce2_topsapp.solid_earth_tides import update_gunw_with_solid_earth_tide
+from isce2_topsapp.solid_earth_tides import update_gunw_with_solid_earth_tide, get_azimuth_time_array
 
 
 def test_set_workflow(isce_data_dir_for_set,
@@ -27,3 +30,27 @@ def test_set_workflow(isce_data_dir_for_set,
             # Transform should not be the identity
             t = ds.transform
             assert t != Affine(1, 0, 0, 0, 1, 0)
+
+
+def test_azimuth_time(isce_data_dir_for_set, gunw_path_for_set):
+    group = 'science/grids/imagingGeometry'
+    with xr.open_dataset(gunw_path_for_set, group=group) as ds:
+        lon_res, lat_res = ds.rio.resolution()
+        lat_res = -lat_res
+
+        # Need upper left corner rather than pixel center
+        hgt = ds.heightsMeta.data
+        lat = ds.latitudeMeta.data + lat_res / 2.
+        lon = ds.longitudeMeta.data - lon_res / 2.
+
+    hgt_mesh, lat_mesh, lon_mesh = np.meshgrid(hgt, lat, lon, indexing='ij')
+    # Azimuth time array
+    X = get_azimuth_time_array(product_dir=isce_data_dir_for_set / 'reference',
+                               height_mesh_arr=hgt_mesh,
+                               latitude_mesh_arr=lat_mesh,
+                               longitude_mesh_arr=lon_mesh)
+    # Total seconds from minimum time
+    Y = (X - X.min()) / np.timedelta64(1, 's')
+    # Vertical standard deviation
+    Y_std = Y.std(axis=0)
+    assert_almost_equal(Y_std, 0, decimal=3)
