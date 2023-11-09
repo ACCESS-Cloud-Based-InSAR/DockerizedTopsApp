7ca9f8a80bf4093d53e510e817350694f011e521
Charlie Marshak <charlie.z.marshak@jpl.nasa.gov>
2023-11-08 16:47:59 -0800

    fix __init__ lint

--- isce2_topsapp/__init__.py
+++ isce2_topsapp/__init__.py
@@ -3,14 +3,15 @@ from importlib.metadata import PackageNotFoundError, version
 
 from isce2_topsapp.delivery_prep import prepare_for_delivery
 from isce2_topsapp.localize_aux_cal import download_aux_cal
-from isce2_topsapp.localize_burst import BurstParams, download_bursts, get_region_of_interest
+from isce2_topsapp.localize_burst import (BurstParams, download_bursts,
+                                          get_region_of_interest)
 from isce2_topsapp.localize_dem import download_dem_for_isce2
 from isce2_topsapp.localize_mask import download_water_mask
 from isce2_topsapp.localize_orbits import download_orbits
 from isce2_topsapp.localize_slc import download_slcs, get_asf_slc_objects
 from isce2_topsapp.packaging import package_gunw_product
-from isce2_topsapp.topsapp_proc import topsapp_processing
 from isce2_topsapp.topsapp_params import topsappParams
+from isce2_topsapp.topsapp_proc import topsapp_processing
 
 try:
     __version__ = version(__name__)
