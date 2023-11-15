0e023d870227530a94f292367e184d3b16b591fb
Forrest Williams <ffwilliams2@alaska.edu>
2023-11-15 16:12:55 -0600

    remove unused regex:

--- tests/test_main.py
+++ tests/test_main.py
@@ -126,7 +126,7 @@ def test_check_esa_credentials_missing(tmp_path, monkeypatch):
         m.setenv('ESA_PASSWORD', 'env_password')
         m.setenv('HOME', str(tmp_path))
         (tmp_path / '.netrc').write_text('')
-        msg = 'Please provide.* --esa-username .*'
+        msg = 'Please provide.*'
         with pytest.raises(ValueError, match=msg):
             check_esa_credentials(None, None)
 
