8576c074976e8d4cef3dd4b7bac88e1bb05ebf41
Forrest Williams <ffwilliams2@alaska.edu>
2023-11-15 08:30:29 -0600

    add esa credential test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,8 @@
+import os
+
 import pytest
 
-from isce2_topsapp.__main__ import ensure_earthdata_credentials, esd_threshold_argument, true_false_string_argument
+from isce2_topsapp.__main__ import check_esa_credentials, ensure_earthdata_credentials, esd_threshold_argument, true_false_string_argument
 
 
 def test_main_check_earthdata_credentials_prefer_netrc(tmp_path, monkeypatch):
@@ -73,6 +75,37 @@ def test_main_check_earthdata_credentials_env_variables(tmp_path, monkeypatch):
     assert netrc.read_text() == 'machine foobar.nasa.gov login fizz password baz'
 
 
+@pytest.mark.parametrize('username, password', [('foo', 'bar'), (None, 'bar'), ('foo', None)])
+def test_check_esa_credentials(monkeypatch, username, password):
+    with monkeypatch.context() as m:
+        m.setenv('ESA_USERNAME', 'env_username')
+        m.setenv('ESA_PASSWORD', 'env_password')
+
+        check_esa_credentials(username, password)
+
+        expected_username = username if username is not None else 'env_username'
+        expected_password = password if password is not None else 'env_password'
+
+        assert os.environ['ESA_USERNAME'] == expected_username
+        assert os.environ['ESA_PASSWORD'] == expected_password
+
+
+def test_check_esa_credentials_missing(monkeypatch):
+    with monkeypatch.context() as m:
+        m.delenv('ESA_USERNAME', raising=False)
+        m.setenv('ESA_PASSWORD', 'env_password')
+        msg = 'Please provide.* --esa-username .*'
+        with pytest.raises(ValueError, match=msg):
+            check_esa_credentials(None, None)
+
+    with monkeypatch.context() as m:
+        m.setenv('ESA_USERNAME', 'env_username')
+        m.delenv('ESA_PASSWORD', raising=False)
+        msg = 'Please provide.* --esa-password .*'
+        with pytest.raises(ValueError, match=msg):
+            check_esa_credentials(None, None)
+
+
 def test_true_false_string_argument():
     assert true_false_string_argument('true') is True
     assert true_false_string_argument('TRUE') is True
@@ -90,11 +123,11 @@ def test_true_false_string_argument():
 
 
 def test_esd_threshold_argument():
-    assert esd_threshold_argument('-1') == -1.
-    assert esd_threshold_argument('0') == 0.
+    assert esd_threshold_argument('-1') == -1.0
+    assert esd_threshold_argument('0') == 0.0
     assert esd_threshold_argument('.25') == 0.25
     assert esd_threshold_argument('0.5') == 0.5
-    assert esd_threshold_argument('1') == 1.
+    assert esd_threshold_argument('1') == 1.0
 
     with pytest.raises(ValueError):
         esd_threshold_argument('-1.1')
