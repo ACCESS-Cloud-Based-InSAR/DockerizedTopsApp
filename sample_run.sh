8589d54e855fc7a32aa76eeacf36c824f158f010
Charlie Marshak <charlie.z.marshak@jpl.nasa.gov>
2023-11-16 16:37:19 -0800

    update sample run

--- sample_run.sh
+++ sample_run.sh
@@ -2,8 +2,9 @@ isce2_topsapp --username <username> \
               --password <password> \
               --esa-username <esa-username> \
               --esa-password <esa-password> \
-              --reference-scenes S1B_IW_SLC__1SDV_20210723T014947_20210723T015014_027915_0354B4_B3A9 \
-              --secondary-scenes S1B_IW_SLC__1SDV_20210711T014922_20210711T014949_027740_034F80_859D \
-                                 S1B_IW_SLC__1SDV_20210711T014947_20210711T015013_027740_034F80_D404 \
-                                 S1B_IW_SLC__1SDV_20210711T015011_20210711T015038_027740_034F80_376C \
+              --reference-scenes S1A_IW_SLC__1SDV_20220212T222803_20220212T222830_041886_04FCA3_2B3E \
+                                 S1A_IW_SLC__1SDV_20220212T222828_20220212T222855_041886_04FCA3_A3E2  \
+              --secondary-scenes S1A_IW_SLC__1SDV_20220131T222803_20220131T222830_041711_04F690_8F5F \
+                                 S1A_IW_SLC__1SDV_20220131T222828_20220131T222855_041711_04F690_28D7  \
+              --frame-id 25502 \
              > topsapp_img.out 2> topsapp_img.err
