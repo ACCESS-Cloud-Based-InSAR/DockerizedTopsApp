886c5e61bf4ed97dc321434e37bd8ebdab1e14aa
Charlie Marshak <charlie.z.marshak@jpl.nasa.gov>
2024-01-29 17:51:19 -0800

    mean layers

--- tests/test_packaging.py
+++ tests/test_packaging.py
@@ -1,7 +1,14 @@
 import json
 from pathlib import Path
 
-from isce2_topsapp.packaging import DATASET_VERSION, get_gunw_id, read_baselines
+from numpy.testing import assert_almost_equal
+
+from isce2_topsapp.packaging import (
+    DATASET_VERSION,
+    get_gunw_id,
+    get_layer_stats,
+    read_baselines,
+)
 
 test_dir = Path(__file__).parent
 
@@ -44,3 +51,19 @@ def test_extract_baselines(tops_proc_xml_path):
     }
 
     assert baseline_dict == expected_output
+
+
+def test_mean_of_geocoded_isce_outputs():
+    """This uses the data in the public bucket to demonstrate and verify that the layer means are correct"""
+    out = get_layer_stats(
+        merged_dir="https://gunw-development-testing.s3.us-west-2.amazonaws.com/sample_merged_data"
+    )
+    expected_out = {
+        "mean_filtered_coherence_without_water_mask": 0.3126283,
+        "mean_filtered_coherence_with_water_mask": 0.32342613,
+        "mean_incidence_angle": 28.281992,
+        "mean_azimuth_angle": -99.18769836425781,
+    }
+
+    for key in expected_out:
+        assert_almost_equal(expected_out[key], out[key], decimal=5)
