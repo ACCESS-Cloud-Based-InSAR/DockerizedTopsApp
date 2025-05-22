440002f94dacf490eab243e77f779212290ecf20
Simran S Sangha <sssangha@ucla.edu>
2025-05-22 14:08:24 -0700

    Push flake8 fixes

--- isce2_topsapp/localize_dem.py
+++ isce2_topsapp/localize_dem.py
@@ -1,16 +1,17 @@
 import subprocess
 from pathlib import Path
+from typing import Optional
 
 import numpy as np
 import rasterio
-from typing import Optional
+from lxml import etree
+from shapely.geometry import box
+
 from dem_stitcher.rio_tools import (
     reproject_arr_to_match_profile,
     update_profile_resolution,
 )
 from dem_stitcher.stitcher import stitch_dem
-from lxml import etree
-from shapely.geometry import box
 
 
 def tag_dem_xml_as_ellipsoidal(dem_path: Path) -> str:
