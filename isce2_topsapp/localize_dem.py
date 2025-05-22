061992e808eab19cf86087c97001538a3d28a0c1
Simran S Sangha <sssangha@ucla.edu>
2025-05-22 14:29:53 -0700

    localize_dem flake8 import changes attempt 3

--- isce2_topsapp/localize_dem.py
+++ isce2_topsapp/localize_dem.py
@@ -4,12 +4,12 @@ from typing import Optional
 
 import numpy as np
 import rasterio
-from lxml import etree
 from dem_stitcher.rio_tools import (
     reproject_arr_to_match_profile,
     update_profile_resolution,
 )
 from dem_stitcher.stitcher import stitch_dem
+from lxml import etree
 from shapely.geometry import box
 
 
