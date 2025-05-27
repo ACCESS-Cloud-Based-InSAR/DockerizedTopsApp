cb31dee8eb0c7de35cfe64cbb64f9420961b1d22
Andrew Johnston <asjohnston@alaska.edu>
2025-05-27 15:30:29 -0800

    expand enumeration tests

--- tests/test_enumeration.py
+++ tests/test_enumeration.py
@@ -1,11 +1,16 @@
 from datetime import date
 
+import pytest
+
 from isce2_topsapp import enumeration
 
 
 # TODO test helper functions
 
 def test_get_slcs_by_date_and_frame():
+    with pytest.raises(ValueError):
+        enumeration.get_slcs_for_date_and_frame(date(2018, 2, 17), 16584)
+
     assert enumeration.get_slcs_for_date_and_frame(date(2018, 2, 18), 16584) == [
         'S1A_IW_SLC__1SDV_20180218T003445_20180218T003512_020654_0235ED_81FB',
         'S1A_IW_SLC__1SDV_20180218T003420_20180218T003447_020654_0235ED_D95D',
@@ -19,4 +24,11 @@ def test_get_slcs_by_date_and_frame():
         'S1B_IW_SLC__1SDV_20201222T151943_20201222T152010_024817_02F3D5_4D14',
         'S1B_IW_SLC__1SDV_20201222T151918_20201222T151945_024817_02F3D5_F283',
     ]
-    # TODO add test cases for single-pol, crossing midnight, no scenes found
+    assert enumeration.get_slcs_for_date_and_frame(date(2025, 5, 18), 18830) == [
+        'S1C_IW_SLC__1SDV_20250519T000015_20250519T000042_002392_005070_1A43',
+        'S1C_IW_SLC__1SDV_20250518T235950_20250519T000017_002392_005070_A436',
+    ]
+    assert enumeration.get_slcs_for_date_and_frame(date(2025, 5, 19), 18830) == [
+        'S1C_IW_SLC__1SDV_20250519T000015_20250519T000042_002392_005070_1A43',
+        'S1C_IW_SLC__1SDV_20250518T235950_20250519T000017_002392_005070_A436',
+    ]
